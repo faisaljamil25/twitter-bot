ac3327076b64efa333457f166af6dedbafaf0d28
Faisal Jamil <faisaljamilcs@gmail.com>
2020-11-09 18:13:07 +0530

    feat: like and reply to mentions with hashtags

--- main.py
+++ main.py
@@ -9,15 +9,51 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'Biden2020'
-nrTweets = 10
+FILE_NAME = 'last_seen_id.txt'
 
-for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
-    try:
-        print('Tweet Liked')
-        tweet.favorite()
-        time.sleep(10)
-    except tweepy.TweepError as e:
-        print(e.reason)
-    except StopIteration:
-        break
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...')
+    """ 
+    DEV NOTE: use 1325482760996151297 for testing.
+    NOTE: We need to use tweet_mode='extended' below to show
+    all full tweets (with full_text). Without it, long tweets
+    would be cut off.
+    """
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(
+        last_seen_id,
+        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found #helloworld!')
+            print('responding back...')
+            try:
+                mention.favorite()
+                print('Tweet Liked')
+                api.update_status('@' + mention.user.screen_name +
+                                  '#HelloWorld back to you!', mention.id)
+            except tweepy.TweepError as e:
+                print(e.reason)
+
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
